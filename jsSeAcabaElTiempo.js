6283e64772e4d9e523e37ca7ec8656b53f554170
Javier Ramirez <jramir11@bancoestado.cl>
2022-10-24 19:12:14 -0300

    commit inicial

---
+++ jsSeAcabaElTiempo.js
@@ -0,0 +1,47 @@
+/*function getSecondsSinceStartOfDay() {
+    return new Date().getSeconds() + 
+      new Date().getMinutes() * 60 + 
+      new Date().getHours() * 3600;
+  }
+      
+  setInterval( function() {
+      var time = getSecondsSinceStartOfDay();
+      console.log(time);
+
+      d = new Date();
+      hr = d.getHours();
+    min = d.getMinutes();
+    sec = d.getSeconds();
+    hr_rotation = 30 * hr + min / 2; //converting current time
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+
+  }, 1000);*/
+  
+
+
+  let inc = 1000;
+
+  clock();
+
+function clock() {
+  const date = new Date();
+
+  const hours = ((date.getHours() + 11) % 12 + 1);
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  
+  const hour = hours * 30;
+  const minute = minutes * 6;
+  const second = seconds * 6;
+  
+  document.querySelector('#hour').style.transform = `rotate(${hour}deg)`
+  document.querySelector('#minutes').style.transform = `rotate(${minute}deg)`
+  document.querySelector('#seconds').style.transform = `rotate(${second}deg)`
+}
+
+setInterval(clock, inc);
